487efd12085ff55f58fc4a5e747ae4606765d5de
pheoohyeeze <104685492+pheoohyeeze@users.noreply.github.com>
2025-10-04 01:50:25 +0700

    Delete node-functions directory

--- node-functions/hello-pages.js
+++
@@ -1,7 +0,0 @@
-// hello-pages.js - 示例页面路由
-export default function helloPages(req, res) {
-  return {
-    message: "Hello from pages route!",
-    path: "/hello-pages"
-  };
-}
--- node-functions/helloworld.js
+++
@@ -1,7 +0,0 @@
-// helloworld.js - 示例路由
-export default function helloWorld(req, res) {
-  return {
-    message: "Hello World!",
-    path: "/helloworld"
-  };
-}
